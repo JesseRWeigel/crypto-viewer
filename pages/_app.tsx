68025eed2be18cc376188bbddeeb47664bde2526
Jesse Weigel <jesseweigel@gmail.com>
2021-08-04 21:46:06 -0400

    feat: add basic project features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 export default MyApp
